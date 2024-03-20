ef5ceeddc0184444cd02227e8561cefd15d6943a
hazel <bguoac@connect.ust.hk>
2024-03-20 23:36:00 +0800

    debugging

--- other_pages/Question_History.py
+++ other_pages/Question_History.py
@@ -92,8 +92,8 @@ else:
                 if questions.loc[i, 'status'] == "sent":
                     write_answer = st.button(label = "Answer")
                     if write_answer:
-                        switch_page("Write")
                         st.query_params["question_id"] = questions.loc[i, 'postid']
+                        switch_page("Write")
                 st.button(label = "View")
         st.write(" ")
 
